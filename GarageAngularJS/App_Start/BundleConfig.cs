b24eb2a5b1543459d79c8443e9fd9994aed3bd34
Gommash <a.lekedal@gmail.com>
2017-06-19 12:01:17 +0200

    Angular is added

--- GarageAngularJS/App_Start/BundleConfig.cs
+++ GarageAngularJS/App_Start/BundleConfig.cs
@@ -8,6 +8,10 @@ namespace GarageAngularJS
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                        "~/Scripts/Angular/angular.min.js",
+                        "~/Scripts/Angular/garage-angular.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
